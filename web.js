2003db966422059e037c507fab05c42cfb047048
benjaminbreuil <benjamin.breuil@jobtrek.ch>
2022-10-26 11:20:44 +0200

    random color

--- web.js
+++ web.js
@@ -8,7 +8,8 @@ flexbox1.innerText = 'salut les filles'
 let articles = document.getElementsByTagName('article')
 
 for (const article of articles) {
-    article.style.backgroundColor = 'red'
+    article.style.backgroundColor = '#' +  Math.floor(Math.random()*16777215).toString(16);
+
 }
 
 })
