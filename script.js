868dd0caf97a385d8298b96cf2cf6b244cd858ba
benjaminbreuil <benjamin.breuil@jobtrek.ch>
2022-10-26 09:45:01 +0200

    average function

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("ça fonctionne")
+
+let x = 5
+
+const t =  0
+
+let o = {
+    name: "Benjamin",
+    age: 17,
+
+}
+
+console.log(o.age)
+
+let test = [5,4,3]
+
+function average(tableau) {
+    let total = 0
+
+    for (const note of tableau) {
+        total+=note
+    }
+
+    return total / tableau.length
+
+}
+
+console.log(average(test))
+
